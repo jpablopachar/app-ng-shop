bf5ffd0bd37072599c2063e875769f85364719e1
pablo.pachar <pablo.pachar@exmerdev.com>
2022-01-08 12:53:00 -0500

    Se instala ngrx para adminitrar la sesion del usuario

--- client/libs/users/src/lib/users.module.ts
+++ client/libs/users/src/lib/users.module.ts
@@ -7,6 +7,11 @@ import { InputTextModule } from 'primeng/inputtext';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthService } from './services/auth.service';
 import { LocalstorageService } from './services/localstorage.service';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUsers from './state/users.reducer';
+import { UsersEffects } from './state/users.effects';
+import { UsersFacade } from './state/users.facade';
 
 export const usersRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
@@ -20,8 +25,10 @@ export const usersRoutes: Route[] = [
     ReactiveFormsModule,
     InputTextModule,
     ButtonModule,
+    StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
+    EffectsModule.forFeature([UsersEffects]),
   ],
   declarations: [LoginComponent],
-  providers: [AuthService, LocalstorageService]
+  providers: [AuthService, LocalstorageService, UsersFacade],
 })
 export class UsersModule {}
